e576a4352e2ecbb2bb122335a747535a45d647ca
ryota-09 <n.ryo950911@gmail.com>
2022-02-09 17:06:18 +0900

    従業員の詳細ページ完成

--- src/views/EmployeeDetail.vue
+++ src/views/EmployeeDetail.vue
@@ -1,6 +1,100 @@
 <template>
   <div>
-    
+    <div class="top-wrapper">
+      <div class="container">
+        <div class="row">
+          <form action="employeeList.html">
+            <fieldset>
+              <legend>従業員情報</legend>
+              <table>
+                <tr>
+                  <th nowrap>従業員名</th>
+                  <td>
+                    <span>{{ currentEmployee.name }}</span>
+                  </td>
+                </tr>
+                <tr>
+                  <th nowrap>写真</th>
+                  <td>
+                    <img v-bind:src="currentEmployeeImage" />
+                  </td>
+                </tr>
+                <tr>
+                  <th nowrap>性別</th>
+                  <td>
+                    <span>{{ currentEmployee.gender }}</span>
+                  </td>
+                </tr>
+                <tr>
+                  <th nowrap>入社日</th>
+                  <td>
+                    <span>{{ currentEmployee.hireDate }}</span>
+                  </td>
+                </tr>
+                <tr>
+                  <th nowrap>メールアドレス</th>
+                  <td>
+                    <span>{{ currentEmployee.mailAddress }}</span>
+                  </td>
+                </tr>
+                <tr>
+                  <th nowrap>郵便番号</th>
+                  <td>
+                    <span>{{ currentEmployee.zipCode}}</span>
+                  </td>
+                </tr>
+                <tr>
+                  <th nowrap>住所</th>
+                  <td>
+                    <span>{{ currentEmployee.address }}</span>
+                  </td>
+                </tr>
+                <tr>
+                  <th nowrap>電話番号</th>
+                  <td>
+                    <span>{{ currentEmployee.telephone }}</span>
+                  </td>
+                </tr>
+                <tr>
+                  <th nowrap>給料</th>
+                  <td>
+                    <span>{{ currentEmployee.salary }}円</span>
+                  </td>
+                </tr>
+                <tr>
+                  <th nowrap>特性</th>
+                  <td>
+                    <span
+                      >{{ currentEmployee.characteristics }}</span
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <th nowrap>扶養人数</th>
+                  <td>
+                    <div class="input-field col s12">
+                      <div class="error">エラーメッセージ</div>
+                      <input
+                        id="dependentsCount"
+                        type="text"
+                        class="validate"
+                        value="3"
+                        required
+                      />
+                      <label for="dependentsCount2">扶養人数</label>
+                    </div>
+                  </td>
+                </tr>
+              </table>
+
+              <button class="btn btn-register waves-effect waves-light">
+                更新
+              </button>
+            </fieldset>
+          </form>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
