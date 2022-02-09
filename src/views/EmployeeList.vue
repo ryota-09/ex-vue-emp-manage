60a17278282b2b1bcac505918dba2bd72af544ae
ryota-09 <n.ryo950911@gmail.com>
2022-02-09 15:16:30 +0900

    従業員一覧を表示するためのscriptタグの設定

--- src/views/EmployeeList.vue
+++ src/views/EmployeeList.vue
@@ -5,10 +5,32 @@
 </template>
 
 <script lang="ts">
+import { Employee } from "@/types/employee";
 import { Component, Vue } from "vue-property-decorator";
 @Component
 export default class EmployeeList extends Vue {
 
+  /**
+   * Vuexストアのアクション経由で非同期でWebAPIから従業員一覧を取得する.
+   */
+  created(): void{
+    this.$store.dispatch("getEmployeeList")
+  }
+
+  /**
+   * 非同期で取得したVuexストア内の従業員数を取得し返す
+   * @returns 従業員数をreturnする
+   */
+  get employeeCount(): number{
+    return this.$store.getters.getEmployeeCount;
+  }
+  /**
+   * 非同期で取得したVuexストア内の従業員一覧を取得し返す
+   * @returns 従業員一覧の配列をreturnする
+   */
+  get employees(): Array<Employee>{
+    return this.$store.getters.getEmployees;
+  }
 }
 </script>
 
