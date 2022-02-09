766273e1b59c8bc0cee713161f979a80e512b6ce
ryota-09 <n.ryo950911@gmail.com>
2022-02-09 12:00:30 +0900

    登録画面のtemplate完成

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,11 @@
 /**
- * 従業員情報を一元管理するためのストア.
- * 
- * @remarks
+ * 共通情報を一元管理するためのストア.
  *
  */
 import Vue from "vue";
 import Vuex from "vuex";
-import { Employee } from "@/types/employee"
-import axios from 'axios'
+import { Employee } from "@/types/employee";
+import axios from "axios";
 
 Vue.use(Vuex);
 
@@ -17,14 +15,14 @@ export default new Vuex.Store({
     //従業員数
     totalEmployeeCount: 0,
     //従業員一覧
-    employees: new Array<Employee>()
+    employees: new Array<Employee>(),
   }, // end state
   /**
-     * 従業員一覧情報をWebAPIから取得してmutationを呼び出す.
-     * @param context 
-     */
+   * 従業員一覧情報をWebAPIから取得してmutationを呼び出す.
+   * @param context - コンテキスト
+   */
   actions: {
-    async getEmployeeList(context){
+    async getEmployeeList(context) {
       const response = await axios.get(
         "http://153.127.48.168:8080/ex-emp-api/employee/employees"
       );
@@ -33,19 +31,19 @@ export default new Vuex.Store({
 
       const payload = response.data;
 
-      context.commit("showEmployeeList",payload);
-    }
+      context.commit("showEmployeeList", payload);
+    },
   }, // end actions
   mutations: {
     /**
      * 従業員一覧情報を作成してstateに格納する.
-     * @param state 
-     * @param payload 
+     * @param state - ステイト
+     * @param payload - ペイロード
      */
-    showEmployeeList(state, payload){
+    showEmployeeList(state, payload) {
       state.totalEmployeeCount = payload.totalEmployeeCount;
 
-      for(const employee of payload.employees){
+      for (const employee of payload.employees) {
         state.employees.push(
           new Employee(
             employee.id,
@@ -61,33 +59,43 @@ export default new Vuex.Store({
             employee.characteristics,
             employee.dependentsCount
           )
-        )
+        );
       }
-    }
+    },
   }, // end mutations
   getters: {
     /**
      * 従業員数を返す.
-     * @param state 
-     * @returns 検索IDから絞り込んだ従業員をreturnする
+     * @param state - ステイト
+     * @returns 従業員数
      */
-    getEmployeeCount(state){
+    getEmployeeCount(state) {
       return state.totalEmployeeCount;
     },
-    getEmployees(state){
+    /**
+     * 従業員一覧を返す.
+     * @param state - ステイト
+     * @returns 従業員一覧
+     */
+    getEmployees(state) {
       return state.employees;
     },
-    getEmployeeByID(state){
+    /**
+     * IDから従業員を1件検索し返す.
+     * @param state - ステイト
+     * @returns 絞り込んだEmployees配列の0番目をreturnする
+     */
+    getEmployeeByID(state) {
       return (serchId: number) => {
         const newArray = [];
-        for(const employee of state.employees){
-          if ( employee.id == serchId){
+        for (const employee of state.employees) {
+          if (employee.id === serchId) {
             newArray.push(employee);
           }
         }
-        return newArray[0]
-      }
-    }
+        return newArray[0];
+      };
+    },
   }, // end getters
   modules: {}, // end modules
 });
--- src/views/RegisterAdmin.vue
+++ src/views/RegisterAdmin.vue
@@ -1,6 +1,77 @@
 <template>
   <div>
-    
+    <header>
+      <div class="container">
+        <div class="header">
+          <div class="header-left">
+            <a href="login.html">
+              <img class="logo" src="img/header_logo.png" />
+            </a>
+          </div>
+
+          <div class="header-right">
+            <a href="registerAdmin.html">管理者登録</a>
+          </div>
+        </div>
+      </div>
+    </header>
+    <div class="top-wrapper">
+      <div class="container">
+        <div class="row register-page">
+          <div class="error">エラーメッセージ</div>
+          <form class="col s12" id="reg-form">
+            <div class="row">
+              <div class="input-field col s6">
+                <input id="last_name" type="text" class="validate" v-model="lastName" required />
+                <label for="last_name">姓</label>
+              </div>
+              <div class="input-field col s6">
+                <input id="first_name" type="text" class="validate" v-model="firstName" required />
+                <label for="first_name">名</label>
+              </div>
+            </div>
+            <div class="row">
+              <div class="input-field col s12">
+                <input id="email" type="email" class="validate" v-model="mailAddress" required />
+                <label for="email">メールアドレス</label>
+              </div>
+            </div>
+            <div class="row">
+              <div class="input-field col s12">
+                <input
+                  id="password"
+                  type="password"
+                  class="validate"
+                  minlength="8"
+                  v-model="password"
+                  required
+                />
+                <label for="password">パスワード</label>
+              </div>
+            </div>
+            <div class="row">
+              <div class="input-field col s6">
+                <button
+                  class="btn btn-large btn-register waves-effect waves-light"
+                  v-on:click="registerAdmin"
+                  type="button"
+                >
+                  登録
+                  <i class="material-icons right">done</i>
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+    <footer>
+      <div class="container">
+        <div class="footer">
+          <div class="footer-center">©️ xxx Inc.</div>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
@@ -10,11 +81,13 @@
  */
 import { Component, Vue } from "vue-property-decorator";
 @Component
-export default class XXXComponent extends Vue {
-
+export default class RegisterAdmin extends Vue {
+  private errorMessage = "エラーメッセージ";
+  private lastName = "";
+  private firstName = "";
+  private mailAddress = "";
+  private password = "";
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
